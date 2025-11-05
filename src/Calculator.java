b6053b9c9a4274c93ea7fa1560c0b93e9a882c77
slot013 <slot00013@gmail.com>
2025-11-05 14:42:38 +0300

    Добавлена обработка ошибок и цикл для повторных вычислений

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,63 @@
 import java.util.Scanner;
 
 public class  Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("Введите первое число: ");
+            while (!sc.hasNextDouble()) {
+                System.out.println("Ошибка: введите число!");
+                sc.next();
+            }
+            double a = sc.nextDouble(); // считываем 1 число
 
+
+            System.out.println("Введите операцию (+,-,/,*): ");
+            char op = sc.next().charAt(0); // считываем только 1 символ который отвечает за операцию
+
+
+            System.out.println("Введите второе число: ");
+            while (!sc.hasNextDouble()) {
+                System.out.println("Ошибка: введите число!");
+                sc.next();
+            }
+            double c = sc.nextDouble();
+
+            double result;
+            switch (op) {
+                case '+':
+                    result = a + c;
+                    break;
+
+                case '-':
+                    result = a - c;
+                    break;
+
+                case '/':
+
+                    if (c == 0) {
+                        System.out.println("Ошибка: деление на ноль!");
+                        continue;
+
+                    }
+
+                    result = a / c;
+                    break;
+
+                case '*':
+                    result = a * c;
+                    break;
+
+                default:
+                    System.out.println("Некорректная операция");
+                    return;
+            }
+            System.out.println("Результат: " + result);
+            System.out.print("Хотите продолжить? (y/n): ");
+            if (sc.next().equalsIgnoreCase("n")) break;
+
+
+        }
+    }
 
 }
